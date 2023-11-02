04cf4b91c9c2545caa53de14d91410b86d1ce626
AleixCorres <145031923+AleixCorres@users.noreply.github.com>
2023-11-02 20:15:35 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const gameBoard = document.querySelector("#gameBoard");
 
-const width=7;
+
 const startElements = [
     "","","","","","","",
     "","","","","","","",
@@ -10,13 +10,45 @@ const startElements = [
     "","","","","","","",
     "","","","","","","",
 ];
+
 function createBoard() {
-    startElements.forEach((startElement)=> {
+    startElements.forEach((startElement, i)=> {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.setAttribute('squareid' , i)
         square.innerHTML = startElement;
         gameBoard.append(square);
 
     })
 }
-createBoard()
+
+const gameElements = document.querySelector("#elementsBoard");
+const gamePieces = [generator, solarPanel, wire, windTurbine, connector];
+
+
+function initialElements(){
+    gamePieces.forEach((gamePiece)=> {
+       const piece = document.createElement('div');
+       piece.classList.add('square');
+       piece.innerHTML = gamePiece;
+        gameElements.append(piece);
+
+    })
+    
+}
+
+
+createBoard();
+initialElements();
+
+const allSquares = document.querySelectorAll("#gameBoard .square");
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart' , dragStart);
+})
+
+let startPositionId
+
+function dragStart(e) {
+    console.log(e.target.getAttribute(squareid));
+}
--- elements.js
+++ elements.js
@@ -1,1 +1,5 @@
 const generator = '<div class="piece" id="generator"><img src="img/generator.png"></div>';
+const solarPanel = '<div class="piece" id="solarPanel"><img src="img/solarPanel.png"></div>';
+const wire = '<div class="piece" id="wire"><img src="img/wire.png"></div>';
+const windTurbine = '<div class="piece" id="windTurbine"><img src="img/windTurbine.png"></div>';
+const connector = '<div class="piece" id="connector"><img src="img/connector.png"></div>';
